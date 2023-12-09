5fbd3e139d0667997edf0a1730c8ae1ca06835a2
Evan Igwilo <uvclassic@gmail.com>
2023-12-09 17:54:10 +0000

    CHORE: Cleanup and installing dependencies

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+//  Next.js modules
+import Head from "next/head";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="mb-8">
+      Hello
+    </div>
+  );
+};
+
+export default Home;
